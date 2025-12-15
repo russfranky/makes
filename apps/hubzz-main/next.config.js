ce5d6d68adf4d8d6dbc57f739a2efda47c4f3774
Russ <russ@thingscorp.com>
2025-12-15 15:44:07 -0500

    Update next.config.js

--- apps/hubzz-main/next.config.js
+++ apps/hubzz-main/next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     appDir: true
+  },
+  webpack: (config) => {
+    config.resolve.alias['@shared-utils'] = path.resolve(__dirname, '../../hubzz/packages/shared-utils/src');
+    config.resolve.alias['@config'] = path.resolve(__dirname, '../../hubzz/packages/config/src');
+    return config;
   }
 };
 
